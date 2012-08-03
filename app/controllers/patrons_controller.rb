72758f2b02b7e188c095900fd52854837bcdba34
Adam Wead <amsterdamos@gmail.com>
2012-08-03 16:51:07 -0400

    IT-54: Sorting features and Bootstrap

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -1,8 +1,11 @@
 class PatronsController < ApplicationController
+
+  helper_method :sort_column, :sort_direction
+
   # GET /patrons
   # GET /patrons.json
   def index
-    @patrons = Patron.all
+    @patrons = Patron.order(sort_column + " " + sort_direction)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -38,6 +41,16 @@ class PatronsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to checkin_path, :notice => message }
     end
+  end
+
+  private
+
+  def sort_column
+    Patron.column_names.include?(params[:sort]) ? params[:sort] : "updated_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 
 end
--- app/helpers/patrons_helper.rb
+++ app/helpers/patrons_helper.rb
@@ -1,2 +1,15 @@
 module PatronsHelper
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current " + direction_icon : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
+  def direction_icon(icon = "")
+    icon = "icon-chevron-up" if sort_direction == "asc"
+    icon = "icon-chevron-down" if sort_direction == "desc"
+    return icon
+  end
 end
